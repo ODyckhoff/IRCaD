10c99cde789eeea34f25d7582f115cd8e678f94c
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2014-04-28 01:22:51 +0100

    Inserted skeleton code for movement mechanics

---
+++ game/mechanics/move.h
@@ -0,0 +1,12 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+#include "../environment/exits.h"
+#include "../environment/tile.h"
+#include "../environment/map.h"
+
+struct map_t mapswitch(struct map_t *cur_map, int maptype);
+struct tile_t go(struct tile_t *cur_tile, int dir);
+
+
+#endif /* MOVE_H */
