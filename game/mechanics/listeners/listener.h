adc1ad0118f82ec5176dae96586b27537a186216
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2015-02-25 00:10:11 +0000

    typedef'd empty structs for listeners and listener data of all event types.

--- game/mechanics/listeners/listener.h
+++ game/mechanics/listeners/listener.h
@@ -1,46 +1,41 @@
 #ifndef __LISTENER_H
 #define __LISTENER_H
 
-enum lstnr_e {
-    ACTION,
-    COMBAT,
-    INTERFACE,
-    PARTY,
-    CHARACTER,
-    GAME,
-    INTERNAL,
-    PLAYER,
-    WORLD
-};
+typedef struct action_lstnr_t {
 
-typedef struct lstnr_t {
-    enum lstnr_e lstnrtype;
-    union {
-        struct action_lstnr_t act;
-        struct combat_lstnr_t com;
-        struct iface_lstnr_t  iface;
-        struct party_lstnr_t  party;
-        struct chrctr_lstnr_t chrctr;
-        struct game_lstnr_t   game;
-        struct intrnl_lstnr_t intrnl;
-        struct player_lstnr_t player;
-        struct world_lstnr_t  world;
-    };
-} lstnr_t;
+} action_lstnr_t;
 
-typedef struct lstnrdata_t {
-    enum lstnr_e lstnrtype;
-    union {
-        struct action_lstnrdata_t act;
-        struct combat_lstnrdata_t com;
-        struct iface_lstnrdata_t  iface;
-        struct party_lstnrdata_t  party;
-        struct chrctr_lstnrdata_t chrctr;
-        struct game_lstnrdata_t   game;
-        struct intrnl_lstnrdata_t intrnl;
-        struct player_lstnrdata_t player;
-        struct world_lstnrdata_t  world;
-    };
-} lstnrdata_t;
+typedef struct combat_lstnr_t {
+
+} combat_lstnr_t;
+
+typedef struct iface_lstnr_t {
+
+} iface_lstnr_t;
+
+typedef struct party_lstnr_t {
+
+} party_lstnr_t;
+
+typedef struct chrctr_lstnr_t {
+
+} chrctr_lstnr_t;
+
+typedef struct game_lstnr_t {
+
+} game_lstnr_t;
+
+typedef struct intrnl_lstnr_t {
+
+} intnrl_lstnr_t;
+
+typedef struct player_lstnr_t {
+
+} player_lstnr_t;
+
+typedef struct world_lstnr_t {
+
+} world_lstnr_t;
 
 #endif /* __LISTENER_H */
+
---
+++ game/mechanics/listeners/lstnrdata.h
@@ -0,0 +1,41 @@
+#ifndef __LSTNRDATA_H
+#define __LSTNRDATA_H
+
+typedef struct action_lstnrdata_t {
+
+} action_lstnrdata_t;
+
+typedef struct combat_lstnrdata_t {
+
+} combat_lstnrdata_t;
+
+typedef struct iface_lstnrdata_t {
+
+} iface_lstnrdata_t;
+
+typedef struct party_lstnrdata_t {
+
+} party_lstnrdata_t;
+
+typedef struct chrctr_lstnrdata_t {
+
+} chrctr_lstnrdata_t;
+
+typedef struct game_lstnrdata_t {
+
+} game_lstnrdata_t;
+
+typedef struct intrnl_lstnrdata_t {
+
+} intnrl_lstnrdata_t;
+
+typedef struct player_lstnrdata_t {
+
+} player_lstnrdata_t;
+
+typedef struct world_lstnrdata_t {
+
+} world_lstnrdata_t;
+
+#endif /* __LSTNRDATA_H */
+
